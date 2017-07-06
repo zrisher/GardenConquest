cba4feae54895ad5b5f6ad15bea307cbaecf2c85
zrisher <zrisher@yahoo.com>
2017-07-06 05:23:55 -0700

    build fixes, v 0.0.1

--- Scripts/Properties/VersionInfo.cs
+++ Scripts/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 // Version information for an assembly consists of the following four values:
 //
@@ -10,4 +10,4 @@
 // Major, Minor, and Build should be set in VersionInfo.cs
 // VersionInfo.cs should be set to not compile.
 // Revision is set by se_mod_builder in VersionInfo - User.cs with the rest of the version.
-[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
